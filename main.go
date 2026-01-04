11e994781826d102afaa7f3ed86628e94b148f98
exy <teslawiktor@gmail.com>
2026-01-04 12:17:22 +0100

    SQLite & React

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+type Post struct {
+	gorm.Model
+	Title string
+	Slug string `gorm:"uniqueIndex:idx_slug"`
+	Likes uint
+}
+
+var db, err = gorm.Open(sqlite.Open("sqlite.db"), &gorm.Config{})
 
 func main(){
-	fmt.Println("Hello world")
+	db.AutoMigrate(&Post{})
 }
